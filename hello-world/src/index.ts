c53bbe75addbb48b33cc33bc127267fbe2400b2d
Hisashi-Saeki <161787049+Hisashi-Saeki@users.noreply.github.com>
2024-03-06 15:34:45 +0900

    pluginの追加とprettierによるフォーマット

--- hello-world/src/index.ts
+++ hello-world/src/index.ts
@@ -1,16 +1,20 @@
-import express from 'express';
+import express from "express";
 const app: express.Express = express();
 
-app.use((req: express.Request, res: express.Response, next: express.NextFunction) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
-})
+app.use(
+  (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept",
+    );
+  },
+);
 
-app.get('/', (req: express.Request, res: express.Response) => {
-  res.send('Hello World!');
+app.get("/", (req: express.Request, res: express.Response) => {
+  res.send("Hello World!");
 });
 
 app.listen(3000, () => {
-  console.log('App is running on port 3000');
+  console.log("App is running on port 3000");
 });
-
