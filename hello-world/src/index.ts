bd4f81826342d6da3a9fbc938a6ba1bfb2ac5574
Hisashi-Saeki <161787049+Hisashi-Saeki@users.noreply.github.com>
2024-03-21 11:20:15 +0900

    ポートを3000番に修正

--- hello-world/src/index.ts
+++ hello-world/src/index.ts
@@ -16,6 +16,6 @@ app.get("/", (req: express.Request, res: express.Response) => {
   res.send("Hello World!");
 });
 
-app.listen(80, () => {
-  console.log("App is running on port 80");
+app.listen(3000, () => {
+  console.log("App is running on port 3000");
 });
