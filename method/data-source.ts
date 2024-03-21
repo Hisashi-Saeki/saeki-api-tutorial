ac812dd80d8f1386ff0daf7b8299040ba86f318d
Hisashi-Saeki <161787049+Hisashi-Saeki@users.noreply.github.com>
2024-03-21 12:59:43 +0900

    host名を修正→環境変数

--- method/data-source.ts
+++ method/data-source.ts
@@ -4,7 +4,7 @@ dotenv.config();
 
 export const AppDataSource = new DataSource({
   type: "mysql",
-  host: "127.0.0.1",
+  host: process.env.MYSQL_HOST,
   port: 3306,
   username: "docker",
   password: process.env.MYSQL_PASSWORD,
