081b368aebab1882b4ba04ae9f1ca3e81c001b0e
Hisashi-Saeki <161787049+Hisashi-Saeki@users.noreply.github.com>
2024-03-21 11:30:42 +0900

    mysqlのport修正

--- method/data-source.ts
+++ method/data-source.ts
@@ -5,7 +5,7 @@ dotenv.config();
 export const AppDataSource = new DataSource({
   type: "mysql",
   host: "localhost",
-  port: 33060,
+  port: 3306,
   username: "docker",
   password: process.env.MYSQL_PASSWORD,
   database: "tutorial_db",
